dfcfbdb9c191ff3558ec599ad6f8b14178ac196a
jinithajohnson <jinithajohnson02@gmail.com>
2024-06-10 16:21:24 +0530

    form

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const student=require("./models/student")
 
 const app =express()
 app.use(cors())
